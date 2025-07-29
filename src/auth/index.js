e20e0feb2c5e48d71613d41d678886fd16adcedb
ArthavPatel25 <arthavcpatel@gmail.com>
2025-07-29 01:30:03 -0400

    updated files

--- src/auth/index.js
+++ src/auth/index.js
@@ -10,6 +10,7 @@ if (
   );
 }
 
+
 if (process.env.AWS_COGNITO_POOL_ID && process.env.AWS_COGNITO_CLIENT_ID) {
   module.exports = require('./cognito');
 }
@@ -17,6 +18,7 @@ if (process.env.AWS_COGNITO_POOL_ID && process.env.AWS_COGNITO_CLIENT_ID) {
 else if (process.env.HTPASSWD_FILE && process.NODE_ENV !== 'production') {
   module.exports = require('./basic-auth');
 }
+
 
 else {
   throw new Error('missing env vars: no authorization configuration found');
